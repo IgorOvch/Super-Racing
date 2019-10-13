6d2da2cd6c15417d2fa76b2fc14a1247a0b7b026
IgorOvch <igor-ovchinnikov-2000@mail.ru>
2019-10-13 19:21:47 +0300

    First commit

---
+++ desktop/src/com/mirea/superracing/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mirea.superracing.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mirea.superracing.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
