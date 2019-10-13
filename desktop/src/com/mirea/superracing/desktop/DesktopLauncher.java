b05c9b2fd3f1969172e26c954a448b94b02c6901
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-13 21:31:40 +0300

    LARGE BLACK WINDOW ADDED

--- desktop/src/com/mirea/superracing/desktop/DesktopLauncher.java
+++ desktop/src/com/mirea/superracing/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mirea.superracing.MainClass;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainClass(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Super-Racing";
+		cfg.fullscreen = true;
+		new LwjglApplication(new MainClass(), cfg);
 	}
 }
