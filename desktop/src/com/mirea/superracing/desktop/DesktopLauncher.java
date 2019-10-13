00bd37b13bce73ebfff87ba873e7df4fc3b0ee31
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-13 22:38:08 +0300

    Normal window

--- desktop/src/com/mirea/superracing/desktop/DesktopLauncher.java
+++ desktop/src/com/mirea/superracing/desktop/DesktopLauncher.java
@@ -1,9 +1,12 @@
 package com.mirea.superracing.desktop;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mirea.superracing.MainClass;
-
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,7 +14,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Super-Racing";
-		cfg.fullscreen = true;
+		cfg.height = 800;
+		cfg.width = 1400;
 		new LwjglApplication(new MainClass(), cfg);
 	}
 }
